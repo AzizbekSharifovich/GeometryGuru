667d6ad854fcb35023dab47abb0f342b7c2554cc
AzizbekSharifovich <azizbek0698@gmail.com>
2023-07-25 10:45:50 +0500

    Add Program file

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GeometryGuru
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Uchburchakning a tomonini kiriting:");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Uchburchakning b tomonini kiriting:");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Uchburchakning c tomonini kiriting:");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            double p = (a + b + c) / 2.0;
+            double s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            Console.WriteLine($"Uchburchakning yuzi: {s}");
+        }
+    }
+}
